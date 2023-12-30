bd2548a335888dbc4e2ca67321e36c711f3feff0
이경용 <sleekydz86@icloud.com>
2023-12-30 12:46:53 +0900

    2.config설정
    config으로 설정하는 방법...

---
+++ webflux_multiDB/fluxweb/src/main/java/com/exambasic/fluxweb/config/DBConfig.java
@@ -0,0 +1,29 @@
+package com.exambasic.fluxweb.config;
+
+import io.r2dbc.spi.ConnectionFactories;
+import io.r2dbc.spi.ConnectionFactory;
+import io.r2dbc.spi.ConnectionFactoryOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.r2dbc.connection.R2dbcTransactionManager;
+import org.springframework.transaction.ReactiveTransactionManager;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public ConnectionFactory connectionFactory(){
+        return ConnectionFactories.get(ConnectionFactoryOptions.parse("r2dbc:mysql://59.32.16.42:3306/lonly?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true")
+                .mutate()
+                .option(ConnectionFactoryOptions.USER,"????")
+                .option(ConnectionFactoryOptions.PASSWORD,"비밀번호")
+                .build());
+
+    }
+
+    @Bean
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory){
+
+        return R2dbcTransactionManager(connectionFactory);
+    }
+}
