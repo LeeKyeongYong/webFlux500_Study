4602549d30fd9cf39c61ef2e8a8ff8aaf960bc87
이경용 <sleekydz86@icloud.com>
2023-12-30 13:08:39 +0900

    config Bean충돌 해결
    여러 개의 ConnectionFactory 빈이 존재할 때 발생하는 오류 발생해서
    명시적으로 어떤 ConnectionFactory 빈을 주입할지 지정 처리함

--- webflux_multiDB/fluxweb/src/main/java/com/exambasic/fluxweb/config/DBConfig.java
+++ webflux_multiDB/fluxweb/src/main/java/com/exambasic/fluxweb/config/DBConfig.java
@@ -5,6 +5,7 @@ import io.r2dbc.spi.ConnectionFactory;
 import io.r2dbc.spi.ConnectionFactoryOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.r2dbc.connection.R2dbcTransactionManager;
 import org.springframework.transaction.ReactiveTransactionManager;
 
@@ -12,18 +13,19 @@ import org.springframework.transaction.ReactiveTransactionManager;
 public class DBConfig {
 
     @Bean
+    @Primary
     public ConnectionFactory connectionFactory(){
         return ConnectionFactories.get(ConnectionFactoryOptions.parse("r2dbc:mysql://59.32.16.42:3306/lonly?useSSL=false&useUnicode=true&serverTimezone=Asia/Seoul&allowPublicKeyRetrieval=true")
                 .mutate()
-                .option(ConnectionFactoryOptions.USER,"????")
+                .option(ConnectionFactoryOptions.USER,"아이디")
                 .option(ConnectionFactoryOptions.PASSWORD,"비밀번호")
                 .build());
 
     }
 
     @Bean
-    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory){
+    public ReactiveTransactionManager transactionManager(ConnectionFactory connectionFactory) {
 
-        return R2dbcTransactionManager(connectionFactory);
+        return new R2dbcTransactionManager(connectionFactory);
     }
 }
